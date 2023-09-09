5d255e0299224b2321a2e6fe71202f05f50a8eb6
vlveqoo <vlveqoo@gmail.com>
2023-09-09 23:12:25 +0900

    예제 복사 후 코드분분석 공부

--- solo/quiz/quiz.js
+++ solo/quiz/quiz.js
@@ -6,10 +6,11 @@ const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
+    const styles = e.currentTarget.classList; // 커렌트타켓: 이벤트핸들러가 부착된위치를 의미
     if (styles.contains("decrease")) {
       count--;
       console.log(styles)
+      console.log(e.target.classList)
     } else if (styles.contains("increase")) {
       count++;
     } else {
