1dd9c051656720c970fc0491778b8673e511546b
vlveqoo <vlveqoo@gmail.com>
2023-09-17 00:32:59 +0900

    map과 filter기능 실습

---
+++ solo/quiz/menu.js
@@ -0,0 +1,512 @@
+const menu=[
+    {
+        id: 1,
+        title: '계성초등학교',
+        category: 'private',
+        establish: '사립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 2,
+        title: '서울교육대학교부설초등학교',
+        category: 'national',
+        establish: '국립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 3,
+        title: '서울매헌초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 4,
+        title: '서울반원초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 5,
+        title: '서울반포초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 6,
+        title: '서울방배초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 7,
+        title: '서울방일초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 8,
+        title: '서울방현초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 9,
+        title: '서울서래초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 10,
+        title: '서울서원초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 11,
+        title: '서울서이초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 12,
+        title: '서울서일초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 13,
+        title: '서울서초초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 14,
+        title: '서울신동초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 15,
+        title: '서울신중초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 16,
+        title: '서울양재초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 17,
+        title: '서울언남초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 18,
+        title: '서울우면초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 19,
+        title: '서울우솔초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 20,
+        title: '서울우암초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 21,
+        title: '서울원명초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 22,
+        title: '서울원촌초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 23,
+        title: '서울이수초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 24,
+        title: '서울잠원초등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '초등',
+        img: '../school.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 25,
+        title: '경원중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 26,
+        title: '내곡중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 27,
+        title: '동덕여자중학교',
+        category: 'private',
+        establish: '사립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 28,
+        title: '반포중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 29,
+        title: '방배중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 30,
+        title: '서문여자중학교',
+        category: 'private',
+        establish: '사립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 31,
+        title: '서운중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 32,
+        title: '서일중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 33,
+        title: '서초중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 34,
+        title: '세화여자중학교',
+        category: 'private',
+        establish: '사립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 35,
+        title: '신동중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 36,
+        title: '신반포중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 37,
+        title: '언남중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 38,
+        title: '영동중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 39,
+        title: '원촌중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 40,
+        title: '이수중학교',
+        category: 'public',
+        establish: '공립',
+        emh: '중',
+        img: '../school2.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 41,
+        title: '동덕여자고등학교',
+        category: 'private',
+        establish: '사립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 42,
+        title: '반포고등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 43,
+        title: '상문고등학교',
+        category: 'private',
+        establish: '사립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 44,
+        title: '서문여자고등학교',
+        category: 'private',
+        establish: '사립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 45,
+        title: '서울고등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 46,
+        title: '서울웹툰애니메이션고등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 47,
+        title: '서초고등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 48,
+        title: '세화고등학교',
+        category: 'private',
+        establish: '사립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 49,
+        title: '세화여자고등학교',
+        category: 'private',
+        establish: '사립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 50,
+        title: '양재고등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    },
+    {
+        id: 51,
+        title: '언남고등학교',
+        category: 'public',
+        establish: '공립',
+        emh: '고등',
+        img: '../school3.png',
+        desc: 'lorem ipsum'
+    }
+];
+
+const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn')
+
+// load items
+window.addEventListener('DOMContentLoaded', function(){
+    displayMenuItems(menu);
+});
+
+// filter items
+filterBtns.forEach(function(btn) {
+    btn.addEventListener('click', function (e) {
+    //    console.log(e.currentTarget.dataset.id);
+       const category = e.currentTarget.dataset.id;
+       const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if(menuItem.category === category) {
+            return menuItem;
+        }
+       });
+       if (category === 'all') {
+        displayMenuItems(menu);
+       } else {
+        displayMenuItems(menuCategory);
+       }
+    });
+});
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
+        // console.log(item);
+        return `<article class="menu-item">
+                    <img src=${item.img} class="photo" alt=${item.title}>
+                    <div class="item-info">
+                        <header>
+                            <h4>${item.title}</h4>
+                            <h4 class="establish">${item.establish}</h4>
+                        </header>
+                        <p class="item-text">
+                            강남서초교육지원청 관내 ${item.establish}${item.emh}학교
+                        </p>
+                    </div>
+                </article>`
+    });
+    // console.log(displayMenu);
+    displayMenu = displayMenu.join('');
+    sectionCenter.innerHTML = displayMenu;
+}
+
+
+
